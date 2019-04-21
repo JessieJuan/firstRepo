0179ffdd358af47f81cc772da5ee7e08cdedd98c
apple <599278998@qq.com>
2019-04-21 19:08:40 +0800

    提交FinalClass  final类 方法 变量 和 setter getter

--- JavaProgramming/src/chapter1/lsj/FinalClass.java
+++ JavaProgramming/src/chapter1/lsj/FinalClass.java
@@ -1,12 +1,35 @@
 package chapter1.lsj;
 
-public class FinalClass {
-    int numb = 3;
-    public void doit(){
+// Person 我们给它叫Bean类
+// final类不能被继承。
+public final class FinalClass {
+    final int numb = 3;
+    private String field1;
+    String field2;
+    // 这是一个getter 用来得到private的变量的值。
+    public String getField1() {
+        return field1;
+    }
+
+    //这是一个setter，用来修改private变量的值。
+    public void setField1(String value) {
+        this.field1 = value;
+    }
+
+    public int getNumb() {
+        return numb;
+    }
+
+    // final方法不能被子类override
+    public final void doit(){
     }
     public static void main(String args[]){
-        FinalClass finalObj = new FinalClass();
-        finalObj.numb++;
+        final FinalClass finalObj = new FinalClass();
+        // final variable canno be assigned a new value;
+        //        finalObj.numb++;
         System.out.println("numb = "+ finalObj.numb);
+        // final 既可以修饰基本变量也可以修饰引用变量
+        // final variable canno be assigned a new value;
+//        finalObj = new FinalClass();
     }
 }
