6bbdf4c524875b46f70a7c8f7411d7684924d7ee
apple <599278998@qq.com>
2019-04-23 17:29:37 +0800

    commit JFrame Notes

--- JavaProgramming/src/chapter1/lsj2/DemoFrame.java
+++ JavaProgramming/src/chapter1/lsj2/DemoFrame.java
@@ -8,6 +8,7 @@ import javax.swing.JLabel;
 public class DemoFrame extends JFrame {
     public static void main(String args[]){
         EventQueue.invokeLater(new Runnable(){
+            // 在事件队列中创建窗体
             public void run(){
                 try{
                     DemoFrame frame = new DemoFrame();
@@ -22,9 +23,14 @@ public class DemoFrame extends JFrame {
     public DemoFrame(){
         super();
         setBounds(100,100,214,140);
+        // 设置窗体的大小位置
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //设置窗体的默认关闭方式
         final JLabel label = new JLabel();
+        //创建JL标签组建
         label.setText("一个JFrame窗体实例");
+        // 设置标签文本
         getContentPane().add(label,BorderLayout.CENTER);
+        // 添加标签组件到窗体容器
     }
 }
